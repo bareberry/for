19f73e4ce1a996ce58a9309883fe8ea1aac3ce9e
bareberry <bareberry@163.com>
2015-07-20 23:14:36 +0800

    First commit

---
+++ src/main/java/sun/beny/module/User.java
@@ -0,0 +1,30 @@
+package sun.beny.module;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: youjie
+ * Date: 15-7-20
+ * Time: 下午11:05
+ * To change this template use File | Settings | File Templates.
+ */
+public class User {
+
+    private String name ;
+    private String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
